2b990f903eebad3cb2ea50768c576596b2a15943
Wright, Christopher R <Christopher.Wright@ca.com>
2018-09-18 17:19:08 -0400

    Add more documentation for imperative overrides
    Signed-off-by: Wright, Christopher R <Christopher.Wright@ca.com>

--- packages/imperative/src/doc/IImperativeOverrides.ts
+++ packages/imperative/src/doc/IImperativeOverrides.ts
@@ -40,7 +40,43 @@ interface IOverridesRestriction {
  * When additional overrides provided by plugins are present, Imperative will favor
  * those classes over ones provided by your application.
  *
- * @TODO ADD MORE DETAILS HERE LATER
+ * Whether you are defining an Imperative Plugin or an Imperative CLI app, all keys in
+ * this object are expected to be a class constructor or of type string.
+ *
+ * - A class constructor will be instantiated directly from imperative with no additional work
+ * by the provider. (Assuming it is properly defined for the specific key)
+ * - A string will trigger imperative to load the module before instantiating it.
+ *     - If the string is absolute, nothing special happens.
+ *     - If the string is relative, then imperative will base the load on a well known location
+ *       depending on if it is a plugin provided override or base cli provided override.
+ *         - For Imperative Plugins: The module is located relative to the packages default import path (e.g `require('package-name')`)
+ *         - For Imperative CLI Apps: The module is located relative to the main entry point of your application
+ *
+ * When defining the location of an overrides as a string, it must adhere to the following format. Otherwise
+ * Imperative will not be able to load the class.
+ *
+ * _Exporting an Anonymous Class_
+ * ```TypeScript
+ * export = class {
+ *   // Code goes here
+ * };
+ * ```
+ *
+ * _Exporting a Named Class_
+ * ```TypeScript
+ * export = class YourOverridesClass {
+ *   // Code goes here
+ * };
+ * ```
+ *
+ * _Using `module.exports` (Not preferred for TypeScript Users)_
+ * ```TypeScript
+ * class YourOverridesClass {
+ *   // Code goes here
+ * }
+ *
+ * module.exports = YourOverridesClass;
+ * ```
  */
 export interface IImperativeOverrides extends IOverridesRestriction {
     /**
