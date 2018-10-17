3b7bd94c68cc831b122e61f95a77d5d1fec508a9
Ly,Lam <lyzla01@ca.com>
2018-10-17 16:46:44 -0400

    fix validate error when plugin path contains spaces
    Signed-off-by: Ly,Lam <lyzla01@ca.com>

--- __tests__/src/packages/imperative/plugins/suites/ValidatePlugin.ts
+++ __tests__/src/packages/imperative/plugins/suites/ValidatePlugin.ts
@@ -34,6 +34,17 @@ describe("Validate plugin", () => {
             expect(result.stdout).toContain("Successfully validated.");
         });
 
+        it("when plugin contain space in path is installed sucessfully", () => {
+            const testPlugin = join(testPluginDir, "space in path plugin");
+            const pluginName: string = "space-in-path-plugin";
+            let result = T.executeTestCLICommand(cliBin, this, ["plugins", "install", testPlugin]);
+            expect(result.stdout).toContain(`Installed plugin name = '${pluginName}'`);
+             const cmd = `plugins validate`;
+            result = T.executeTestCLICommand(cliBin, this, cmd.split(" "));
+            expect(result.stderr).toEqual("");
+            expect(result.stdout).toContain("successfully validated.");
+        });
+
         it("when provided plugin name is installed successfully", () => {
             const pluginName = "normal-plugin";
             const testPlugin = join(testPluginDir, "normal_plugin");
