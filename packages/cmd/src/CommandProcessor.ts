5ce92415c12b6d87fb3efeb51c7bb86b0c0cf11b
ChrisBoehmCA <Christopher.Boehm@ca.com>
2018-10-17 11:19:21 -0400

    revert adding stack to syntax validation error (snapshot issue)
    Signed-off-by: ChrisBoehmCA <Christopher.Boehm@ca.com>

--- packages/cmd/src/CommandProcessor.ts
+++ packages/cmd/src/CommandProcessor.ts
@@ -302,8 +302,7 @@ export class CommandProcessor {
             response.console.error(e.message);
             response.setError({
                 msg: errMsg,
-                additionalDetails: e.message,
-                stack: e.stack
+                additionalDetails: e.message
             });
             response.failed();
             return this.finishResponse(response);
