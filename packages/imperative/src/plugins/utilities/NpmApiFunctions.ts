f582a800a650915a4006c807af41d34090ea41a5
nurra01 <nurra01@ca.com>
2018-10-24 12:51:19 +0200

    Install npm packages globally and with provided prefix, should fix failing tests
    Signed-off-by: nurra01 <nurra01@ca.com>

--- packages/imperative/src/plugins/utilities/NpmApiFunctions.ts
+++ packages/imperative/src/plugins/utilities/NpmApiFunctions.ts
@@ -13,6 +13,8 @@ import {PMFConstants} from "./PMFConstants";
 import {Logger} from "../../../../logger";
 const npm  = require("npm");
 
+const iConsole = Logger.getImperativeLogger();
+
 /**
  * Common function that installs a npm package.
  *
@@ -28,8 +30,6 @@ const npm  = require("npm");
  *
  */
 export async function installPackages(prefix: string, registry: string, global: boolean, npmPackage: string) {
-    const iConsole = Logger.getImperativeLogger();
-
     const npmOptions: object = { prefix, global, registry };
     return new Promise((resolve) => {
         npm.load(npmOptions, (err: Error) => {
@@ -37,21 +37,26 @@ export async function installPackages(prefix: string, registry: string, global: 
                 iConsole.error(err.message);
                 throw new Error(err.message);
             }
+            const globalOrNot = npm.config.get("global");
+            const globalPrefix = npm.config.get("prefix");
             resolve(new Promise((resolveInstall) => {
                 npm.config.set("global-style", true);
+                npm.config.set("global", global);
+                npm.config.set("prefix", PMFConstants.instance.PLUGIN_INSTALL_LOCATION);
                 const npmLog: string[] = [];
                 console.log = (d: string) => {
                     npmLog.push(d);
                     process.stdout.write(d + "\n");
                 };
-                npm.commands.install(PMFConstants.instance.PLUGIN_INSTALL_LOCATION,
-                    [npmPackage], (installError: Error) => {
-                        if (installError) {
-                            iConsole.error(installError.message);
-                            throw new Error(installError.message);
-                        }
-                        resolveInstall(npmLog);
-                    });
+                npm.commands.install([npmPackage], (installError: Error) => {
+                    if (installError) {
+                        iConsole.error(installError.message);
+                        throw new Error(installError.message);
+                    }
+                    resolveInstall(npmLog);
+                });
+                npm.config.set("global", globalOrNot);
+                npm.config.set("prefix", globalPrefix);
             }));
         });
     });
@@ -66,7 +71,8 @@ export async function getRegistry() {
     return new Promise((resolveLoad) => {
         npm.load({}, (err: Error) => {
             if (err) {
-                this.console.error(err.message);
+                iConsole.error(err.message);
+                throw new Error(err.message);
             }
             resolveLoad(new Promise((resolveGetRegistry) => {
                 resolveGetRegistry(npm.config.get("registry"));
