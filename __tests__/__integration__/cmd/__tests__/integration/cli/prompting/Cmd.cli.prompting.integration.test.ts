373e69ddbfe9175cec8dc04213c67031b9f5eee4
ChrisBoehmCA <Christopher.Boehm@broadcom.com>
2019-04-01 09:44:44 -0400

    add prompting integration test using node-pty package
    Signed-off-by: ChrisBoehmCA <Christopher.Boehm@broadcom.com>

---
+++ __tests__/__integration__/cmd/__tests__/integration/cli/prompting/Cmd.cli.prompting.integration.test.ts
@@ -0,0 +1,63 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { ITestEnvironment } from "../../../../../../__src__/environment/doc/response/ITestEnvironment";
+import { SetupTestEnvironment } from "../../../../../../__src__/environment/SetupTestEnvironment";
+import { join } from "path";
+// Test Environment populated in the beforeAll();
+let TEST_ENVIRONMENT: ITestEnvironment;
+describe("cmd-cli profile mapping", () => {
+    // Create the unique test environment
+    beforeAll(async () => {
+        TEST_ENVIRONMENT = await SetupTestEnvironment.createTestEnv({
+            cliHomeEnvVar: "CMD_CLI_CLI_HOME",
+            testName: "cmd_profile_mapping"
+        });
+    });
+
+    afterEach(() => {
+        // delete profiles between tests so that they can be recreated
+        require("rimraf").sync(join(TEST_ENVIRONMENT.workingDir, "profiles"));
+    });
+
+    it("should prompt the user for a value when the default prompt phrase is specified", (done: any) => {
+
+        const myColor = "army green";
+        // for some reason, node-pty won't find "sh" on Windows unless you add .exe
+        const shProgram = require("os").platform() === "win32" ? "sh.exe" : "sh";
+        const ptyProcess = require("node-pty").spawn(shProgram, [join(__dirname, "__scripts__", "prompt_for_color.sh")],
+            {
+                name: "xterm-color",
+                cols: 80,
+                rows: 30,
+                cwd: TEST_ENVIRONMENT.workingDir,
+                env: process.env
+            });
+
+        let output: Buffer = Buffer.alloc(0);
+
+
+        ptyProcess.on("data", (data: string) => {
+            output = Buffer.concat([output, Buffer.from(data)]);
+            ptyProcess.write(myColor + "\n\r");
+        });
+
+        ptyProcess.on("error", (error: any) => {
+            throw error;
+        });
+        ptyProcess.on("end", () => {
+            expect(output.toString()).toContain("Color: " + myColor);
+            done();
+        });
+
+    });
+
+});
