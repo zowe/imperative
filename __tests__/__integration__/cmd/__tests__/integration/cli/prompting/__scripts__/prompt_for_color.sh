2e2acf83b3fe2a3122891981d811c53156240eab
ChrisBoehmCA <Christopher.Boehm@broadcom.com>
2019-04-01 11:13:04 -0400

    remove non working integration test
    Signed-off-by: ChrisBoehmCA <Christopher.Boehm@broadcom.com>

--- __tests__/__integration__/cmd/__tests__/integration/cli/prompting/__scripts__/prompt_for_color.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-# should print the name and type that are specified, not the profile name or type
-cmd-cli profile mapping --mold-type "moldy" --banana-description "big_banana" --color "PROMPT*"
