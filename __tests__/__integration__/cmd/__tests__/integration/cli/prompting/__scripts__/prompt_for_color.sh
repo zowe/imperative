0e3f4b5506954b2da519876e85d06c51753ae6cf
ChrisBoehmCA <Christopher.Boehm@broadcom.com>
2019-04-04 12:12:57 -0400

    restore prompting pty test to test on ubuntu
    Signed-off-by: ChrisBoehmCA <Christopher.Boehm@broadcom.com>

---
+++ __tests__/__integration__/cmd/__tests__/integration/cli/prompting/__scripts__/prompt_for_color.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+set -e
+
+ # should print the name and type that are specified, not the profile name or type
+cmd-cli profile mapping --mold-type "moldy" --banana-description "big_banana" --color "PROMPT*"#!/usr/bin/env bash
