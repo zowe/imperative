80edba851f2e4a29ef43b30e2d113725e0b40b01
Gene Johnston <eugene.johnston@broadcom.com>
2019-07-16 13:01:28 -0400

    Add unit tests for isGuiAvailable().
    Signed-off-by: Gene Johnston <eugene.johnston@broadcom.com>

--- packages/utilities/__tests__/ProcessUtils.test.ts
+++ packages/utilities/__tests__/ProcessUtils.test.ts
@@ -12,31 +12,96 @@
 import { ProcessUtils } from "../../utilities";
 
 describe("ProcessUtils tests", () => {
+    describe("nextTick", () => {
 
-    it("should invoke all next ticks in the proper order", async () => {
-        const chicken = "chicken";
-        const cat = "cat";
-        const dog = "dog";
-        const pig = "pig";
-        const horse = "horse";
+        it("should invoke all next ticks in the proper order", async () => {
+            const chicken = "chicken";
+            const cat = "cat";
+            const dog = "dog";
+            const pig = "pig";
+            const horse = "horse";
 
-        let animal = chicken;
+            let animal = chicken;
 
-        const setCat = () => animal = cat;
-        const setDog = () => animal = dog;
-        const setPig = () => animal = pig;
+            const setCat = () => animal = cat;
+            const setDog = () => animal = dog;
+            const setPig = () => animal = pig;
 
-        await ProcessUtils.nextTick(setCat);
-        expect(animal).toBe(cat);
+            await ProcessUtils.nextTick(setCat);
+            expect(animal).toBe(cat);
 
-        animal = horse;
-        const dogPromise = ProcessUtils.nextTick(setDog);
-        expect(animal).toBe(horse);
+            animal = horse;
+            const dogPromise = ProcessUtils.nextTick(setDog);
+            expect(animal).toBe(horse);
 
-        await dogPromise;
-        expect(animal).toBe(dog);
+            await dogPromise;
+            expect(animal).toBe(dog);
 
-        await ProcessUtils.nextTick(setPig);
-        expect(animal).toBe(pig);
+            await ProcessUtils.nextTick(setPig);
+            expect(animal).toBe(pig);
+        });
     });
+
+    describe("isGuiAvailable", () => {
+        it("should report a GUI on Windows", async () => {
+            if ( process.platform === "win32") {
+                expect(ProcessUtils.isGuiAvailable()).toBe(true);
+            }
+        });
+
+        it("should report no GUI on an ssh connection", async () => {
+            process.env.SSH_CONNECTION = "AnyValue";
+            expect(ProcessUtils.isGuiAvailable()).toBe(false);
+        });
+
+        it("should report a GUI if DISPLAY is set on Linux", async () => {
+            const realPlatform = process.platform;
+            Object.defineProperty(process, "platform", {
+              value: "linux"
+            });
+
+            const realEnv = process.env;
+            Object.defineProperty(process, "env", {
+                value: {
+                    DISPLAY: "xterm"
+                }
+            });
+
+            expect(ProcessUtils.isGuiAvailable()).toBe(true);
+
+            // restore values
+            Object.defineProperty(process, "platform", {
+                value: realPlatform
+            });
+            Object.defineProperty(process, "env", {
+                value: realEnv
+            });
+        });
+
+        it("should report no GUI if DISPLAY is not set on Linux", async () => {
+            const realPlatform = process.platform;
+            Object.defineProperty(process, "platform", {
+              value: "linux"
+            });
+
+            const realEnv = process.env;
+            Object.defineProperty(process, "env", {
+                value: {
+                    DISPLAY: ""
+                }
+            });
+
+            process.env.DISPLAY = "";
+            expect(ProcessUtils.isGuiAvailable()).toBe(false);
+
+            // restore values
+            Object.defineProperty(process, "platform", {
+                value: realPlatform
+            });
+            Object.defineProperty(process, "env", {
+                value: realEnv
+            });
+        });
+    });
+
 });
