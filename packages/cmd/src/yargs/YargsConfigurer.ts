d0e971dbdebc84ed0b199e41f31dd6d81fe5c1d5
Timothy Johnson <timothy.johnson@broadcom.com>
2019-12-10 08:31:01 -0500

    Get help and version commands to work with new yargs
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/cmd/src/yargs/YargsConfigurer.ts
+++ packages/cmd/src/yargs/YargsConfigurer.ts
@@ -65,12 +65,14 @@ export class YargsConfigurer {
             type: "command",
             description: "The command you tried to invoke failed"
         };
+        this.yargs.help(false);
+        this.yargs.version(false);
         this.yargs.showHelpOnFail(false);
         // finally, catch any undefined commands
-        this.yargs.command("*", "Unknown group", (argv: Argv) => {
-                return argv;  // no builder
-            },
-            (argv: any) => {
+        this.yargs.command({
+            command: "*",
+            description: "Unknown group",
+            handler: (argv: any) => {
                 const attemptedCommand = argv._.join(" ");
                 if (attemptedCommand.trim().length === 0) {
                     if (argv.V) {
@@ -151,7 +153,8 @@ export class YargsConfigurer {
                         logger.error("%s", err.msg);
                     });
                 }
-            });
+            }
+        });
 
         this.yargs.fail((msg: string, error: Error, failedYargs: any) => {
             process.exitCode = Constants.ERROR_EXIT_CODE;
