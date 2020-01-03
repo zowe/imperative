85d3c988844fcb6298975e24c5c05ddb0b89b369
Timothy Johnson <timothy.johnson@broadcom.com>
2020-01-03 14:40:25 -0500

    Fix line breaks getting lost during Markdown to HTML conversion
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- packages/cmd/src/help/WebHelpGenerator.ts
+++ packages/cmd/src/help/WebHelpGenerator.ts
@@ -314,7 +314,10 @@ export class WebHelpGenerator {
             markdownContent += "<h4>Commands</h4>\n" + this.buildChildrenSummaryTables(helpGen, rootCommandName + "_" + fullCommandName);
         }
 
-        let htmlContent = "<h2>" + this.genBreadcrumb(rootCommandName, fullCommandName) + this.genPrintButton() + "</h2>\n";
+        // Prevent line breaks from being lost during Markdown to HTML conversion
+        markdownContent = markdownContent.replace(/^(\s+Default value:.+$)(\s+Allowed values:.+$)/gm, "$1\n$2");
+
+        let htmlContent = "<h2>" + this.genBreadcrumb(rootCommandName, fullCommandName) + "</h2>\n";
         htmlContent += this.marked(markdownContent);
 
         // Remove backslash escapes from URLs
