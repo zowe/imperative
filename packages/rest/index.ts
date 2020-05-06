29a4f8f30451c519c1082fd324ba1f8643521727
Gene Johnston <eugene.johnston@broadcom.com>
2020-05-06 19:47:27 -0400

    Exclude packages/rest/index.ts from tslint.
    Signed-off-by: Gene Johnston <eugene.johnston@broadcom.com>

--- packages/rest/index.ts
+++ packages/rest/index.ts
@@ -18,7 +18,12 @@ export * from "./src/client/RestClient";
 export * from "./src/client/RestConstants";
 export * from "./src/client/RestStandAloneUtils";
 export * from "./src/client/RestClientError";
+
+/* The following line is valid, but tslint barfs on it.
+ * So, we exclude this file in imperative/tslint-packages.json.
+ */
 export * as SessConstants from "./src/session/SessConstants";
+
 export * from "./src/session/doc/ISession";
 export * from "./src/session/AbstractSession";
 export * from "./src/session/Session";
