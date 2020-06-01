ef4e107d8e242e9606637c5e20d3f48789f3edcd
Andrew W. Harn <andrew.harn@broadcom.com>
2020-06-01 16:16:54 -0400

    Remove unused imports
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- __tests__/__integration__/cmd/__tests__/integration/cli/auth/Cmd.cli.auth.login.fruit.integration.test.ts
+++ __tests__/__integration__/cmd/__tests__/integration/cli/auth/Cmd.cli.auth.login.fruit.integration.test.ts
@@ -13,8 +13,6 @@ import { runCliScript } from "../../../../../../src/TestUtil";
 import { ITestEnvironment } from "../../../../../../__src__/environment/doc/response/ITestEnvironment";
 import { SetupTestEnvironment } from "../../../../../../__src__/environment/SetupTestEnvironment";
 import { join } from "path";
-import { isMainThread } from "worker_threads";
-import { ImperativeExpect } from "../../../../../../../lib";
 
 // Test Environment populated in the beforeAll();
 let TEST_ENVIRONMENT: ITestEnvironment;
