c6a3395f213b4f122188d4f15829b600ccebcf63
Andrew W. Harn <andrew.harn@broadcom.com>
2020-12-18 14:03:00 +0000

    Fix one more test on Windows
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

--- packages/imperative/__tests__/config/cmd/set/set.handler.test.ts
+++ packages/imperative/__tests__/config/cmd/set/set.handler.test.ts
@@ -783,8 +783,13 @@ describe("Configuration Secure command handler", () => {
         delete compObj.profiles.my_secured.properties.secret;
         compObj.secure = ["profiles.my_secured.properties.secret"]; // Add the secret field to the secrets
 
-        // tslint:disable-next-line: no-magic-numbers
-        expect(keytarDeletePasswordSpy).toHaveBeenCalledTimes(3);
+        if (process.platform === "win32") {
+            // tslint:disable-next-line: no-magic-numbers
+            expect(keytarDeletePasswordSpy).toHaveBeenCalledTimes(4);
+        } else {
+            // tslint:disable-next-line: no-magic-numbers
+            expect(keytarDeletePasswordSpy).toHaveBeenCalledTimes(3);
+        }
         expect(keytarGetPasswordSpy).toHaveBeenCalledTimes(1);
         expect(keytarSetPasswordSpy).toHaveBeenCalledTimes(1);
         expect(keytarSetPasswordSpy).toHaveBeenCalledWith("Zowe", "secure_config_props", fakeSecureDataExpected);
