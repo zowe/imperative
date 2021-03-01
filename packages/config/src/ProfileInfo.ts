158044fff5b6d0a25afd035b2a16e7ecc7b3013b
Gene Johnston <eugene.johnston@broadcom.com>
2021-03-01 09:57:49 -0500

    Skeleton design proposal that compiles.
    Signed-off-by: Gene Johnston <eugene.johnston@broadcom.com>

---
+++ packages/config/src/ProfileInfo.ts
@@ -0,0 +1,249 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { IProfAttrs } from "./doc/IProfAttrs";
+import { IProfArgAttrs } from "./doc/IProfArgAttrs";
+import { IProfMergedArg } from "./doc/IProfMergedArg";
+import { Config } from "./config";
+import { IConfigOpts } from "./doc/IConfigOpts";
+
+/**
+ * This class provides functions to retrieve profile-related information.
+ * It can load the relevant configuration files, merge all possible
+ * profile argument values using the Zowe order-of-precedence, and
+ * access desired profile attributes from the Zowe configuration settings.
+ *
+ * Pseudocode examples:
+ * <pre>
+ *    // Construct a new object. Use it to read the profiles from disk
+ *    profInfo = new ProfileInfo();
+ *    profInfo.readProfilesFromDisk("zowe");
+ *
+ *    // Maybe you want the list of all zosmf profiles
+ *    let arrayOfProfiles = profInfo.getAllProfiles("zosmf");
+ *    youDisplayTheListOfProfiles(arrayOfProfiles);
+ *
+ *    // Maybe you want the default zosmf profile
+ *    let zosmfProfile = profInfo.getDefaultProfile("zosmf");
+ *    youUseTheProfile(zosmfProfile);
+ *
+ *    // Maybe you want the arg values for the default JCLCheck profile
+ *    let jckProfile = profInfo.getDefaultProfile("jclcheck");
+ *    let jckMergedArgs = profInfo.mergeArgsForProfile(jckProfile);
+ *    let jckFinalArgs = youPromptForMissingArgsAndCombineWithKnownArgs(
+ *        jckMergedArgs.knownArgs, jckMergedArgs.missingArgs
+ *    );
+ *    youRunJclCheck(jckFinalArgs);
+ *
+ *    // Maybe no profile of type "zosmf" even exists.
+ *    let zosmfProfiles = profInfo.getAllProfiles("zosmf");
+ *    if (zosmfProfiles.length == 0) {
+ *        // No zosmf profile exists
+ *        // Merge any required arg values for the zosmf profile type
+ *        let zosmfMergedArgs =
+ *            profInfo.mergeArgsForProfileType("zosmf");
+ *
+ *        let finalZosmfArgs =
+ *            youPromptForMissingArgsAndCombineWithKnownArgs
+ *                zosmfMergedArgs.knownArgs,
+ *                zosmfMergedArgs.missingArgs
+ *            );
+ *        youRunSomeZosmfCommand(finalZosmfArgs);
+ *    }
+ *
+ *    // So you want to write to a config file? You must use your own
+ *    // old-school techniques to write to old-school profiles.
+ *    // You then use alternate logic for a team config.
+ *    // You must use the Config API to write to a team configuration.
+ *    // See the Config class documentation for functions to set
+ *    // and save team config arguments.
+ *
+ *    // Let's save some zosmf arguments from the example above.
+ *    let yourZosmfArgsToWrite: IProfArgAttrs =
+ *        youSetValuesToOverwrite(
+ *            zosmfMergedArgs.knownArgs, zosmfMergedArgs.missingArgs
+ *        );
+ *    if (profInfo.usingTeamConfig()) {
+ *        let configObj: Config = profInfo.getTeamConfig();
+ *        youWriteArgValuesUsingConfigObj(
+ *            configObj, yourZosmfArgsToWrite
+ *        );
+ *    } else {
+ *      youWriteOldSchoolProfiles(yourZosmfArgsToWrite);
+ *    }
+ * </pre>
+ */
+export class ProfileInfo {
+    private mLoadedConfig: Config = null;
+    private mUsingTeamConfig: boolean = false;
+
+    // _______________________________________________________________________
+    /**
+     * Get all of the typed profiles in the configuration.
+     *
+     * @param profileType
+     *        Limit selection to only profiles of the specified type.
+     *        If not supplied, the names of all typed profiles are returned.
+     *
+     * @returns An array of profile attribute objects.
+     *          In addition to the name, you get the profile type,
+     *          an indicator of whether the profile is the default profile
+     *          for that type, and the location of that profile.
+     *
+     *          If no profile exists for the specified type (or if
+     *          no profiles of any kind exist), we return an empty array
+     *          ie, length is zero.
+     */
+    public getAllProfiles(profileType?: string): IProfAttrs[] {
+        return [];
+    }
+
+    // _______________________________________________________________________
+    /**
+     * Get the default profile for the specified profile type.
+     *
+     * @param profileType
+     *        The type of profile of interest.
+     *
+     * @returns The default profile. If no profile exists
+     *          for the specified type, we return null;
+     */
+    public getDefaultProfile(profileType: string): IProfAttrs {
+        let defaultProfile: IProfAttrs;
+
+        // todo: Actually implement something
+        const implementSomething: any = null;
+        defaultProfile = implementSomething;
+
+        return defaultProfile;
+    }
+
+    // _______________________________________________________________________
+    /**
+     * Get the Config object used to manipulate the team configuration on disk.
+     *
+     * Our current market direction is to encourage customers to edit the
+     * team configuration files in their favorite text editor.
+     *
+     * If you must ignore this recommended practice, you must use the Config
+     * class to manipulate the team config files. This class has a more detailed
+     * and therefore more complicated API, but it does contain functions to
+     * write data to the team configuration files.
+     *
+     * You must call ProfileInfo.readProfilesFromDisk() before calling this function.
+     *
+     * @returns An instance of the Config class that can be used to manipulate
+     *          the team configuration on disk.
+     */
+    public getTeamConfig(): Config {
+        if (this.mLoadedConfig == null) {
+            throw Error("You must first call ProfileInfo.readProfilesFromDisk().");
+        }
+        return this.mLoadedConfig;
+    }
+    // _______________________________________________________________________
+    /**
+     * Merge all of the available values for arguments defined for the
+     * specified profile. Values are retrieved from the following sources.
+     * Each successive source will override the previous source.
+     * - A default value for the argument that is defined in the profile definition.
+     * - An environment variable for that argument.
+     * - A value defined in the base profile.
+     * - A value defined in the specified service profile.
+     * - For a team configuration, both the base profile values and the
+     *   service profile values will be overridden with values from a
+     *   zowe.config.user.json file (if it exists).
+     *
+     * @param profile
+     *        The profile whose arguments are to be merged.
+     *
+     * @returns An object that contains an array of known profile argument
+     *          values and an array of required profile arguments which
+     *          have no value assigned. Either of the two arrays could be
+     *          of zero length, depending on the user's configuration and
+     *          environment.
+     *
+     *          We will return null if the profile does not exist
+     *          in the current Zowe configuration.
+     */
+    public mergeArgsForProfile(profile: IProfAttrs): IProfMergedArg {
+        let mergedArgs: IProfMergedArg = null;
+
+        // todo: Actually implement something
+        const implementSomething: any = null;
+        mergedArgs = implementSomething;
+
+        return mergedArgs;
+    }
+
+    // _______________________________________________________________________
+    /**
+     * Merge all of the available values for arguments defined for the
+     * specified profile type. See mergeArgsForProfile() for details
+     * about the merging algorithm.
+     * The intended use is when no profile of a specific type exists.
+     * The consumer app can prompt for values for missing arguments
+     * and then perform the desired operation.
+     *
+     * @param profileType
+     *        The type of profile of interest.
+     *
+     * @returns The complete set of required properties;
+     */
+    public mergeArgsForProfileType(profileType: string): IProfMergedArg {
+        let mergedArgs: IProfMergedArg = null;
+
+        // todo: Actually implement something
+        const implementSomething: any = null;
+        mergedArgs = implementSomething;
+
+        return mergedArgs;
+    }
+
+    // _______________________________________________________________________
+    /**
+     * Read either the new team configuration files (if any exist) or
+     * read the old-school profile files.
+     *
+     * todo: Does our consumer need to call this function for old-school profiles?
+     *
+     * @param appName
+     *        The name of the application (like "zowe" in zowe.config.json)
+     *        whose configuration we want to read.
+     *
+     * @param teamCfgOpts
+     *        The optional choices used when reading a team configuration.
+     *        This parameter is ignored, if the end-user is using old-school
+     *        profiles.
+     *        todo: We must add a startingProjectSearchDir to IConfigOpts.
+     */
+    public async readProfilesFromDisk(appName: string, teamCfgOpts?: IConfigOpts) {
+        this.mLoadedConfig = await Config.load(appName, teamCfgOpts);
+        if ( "todo: WeWriteCodeToDetermineThatWeAreUsingTeamConfig") {
+            this.mUsingTeamConfig = true;
+        }
+    }
+
+    // _______________________________________________________________________
+    /**
+     * Retuns an indicator of whether we are using a team configuration or
+     * old-school profiles.
+     *
+     * You must call ProfileInfo.readProfilesFromDisk() before calling this function.
+     *
+     * @returns True when we are using a team config. False means old-school profiles.
+     */
+    public usingTeamConfig(): boolean {
+        if (this.mLoadedConfig == null) {
+            throw Error("You must first call ProfileInfo.readProfilesFromDisk().");
+        }
+        return this.mUsingTeamConfig;
+    }}
---
+++ packages/config/src/doc/IProfMergedArg.ts
@@ -0,0 +1,36 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { IProfArgAttrs } from "./IProfArgAttrs";
+
+/**
+ * The result object from mergeProfileArgs().
+ */
+export interface IProfMergedArg {
+    /**
+     * The list of arguments with known values.
+     * All of the attributes in IProfPropAttrs will be filled in except
+     * when knownArgs[i].propLoc.osLoc (and/or jsonLoc) are not
+     * relevant for the type of location (locType).
+     */
+    knownArgs: IProfArgAttrs[];
+
+    /**
+     * The list of required arguments for which no value has been specified.
+     * Obviously, the missingArgs[i].propValue attribute will not exist.
+     * Note that a generated team configuration template can contain some
+     * arguments with an empty string as a value. Such arguments will be
+     * contained in this missing list. The other missing arguments will
+     * have a missingArgs[i].propLoc, derived from the location of the
+     * profile specified to the function mergeProfileArgs().
+     */
+    missingArgs: IProfArgAttrs[];
+}
