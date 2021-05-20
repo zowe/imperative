e012fa3cf44d5d5f6893a57691d8a6d640f188af
Gene Johnston <eugene.johnston@broadcom.com>
2021-05-20 16:25:59 -0400

    Remove deprecated suppressReport.
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/error/__tests__/ImperativeError.test.ts
+++ packages/error/__tests__/ImperativeError.test.ts
@@ -13,30 +13,10 @@ import { ImperativeError } from "../src/ImperativeError";
 import * as chalk from "chalk";
 
 describe("ImperativeError", () => {
-    it("should deprecate suppressReport", () => {
-        jest.spyOn(console, "warn").mockImplementation(() => { return; });
-
-        /* tslint:disable:no-console */
-        // tslint:disable-next-line
-        new ImperativeError({msg: "test"}, {suppressReport: false });
-
-        expect(console.warn).toHaveBeenCalledWith(expect.stringContaining("[DEPRECATED]"));
-
-        (console.warn as any).mockRestore();
-
-        jest.spyOn(console, "warn").mockImplementation(() => { return; });
-
-        // tslint:disable-next-line
-        new ImperativeError({msg: "test"}, {suppressReport: true });
-
-        expect(console.warn).toHaveBeenCalledWith(expect.stringContaining("[DEPRECATED]"));
-
-        (console.warn as any).mockRestore();
-    });
-
     it("should not throw any deprecation warnings", () => {
         jest.spyOn(console, "warn").mockImplementation(() => { return; });
 
+        /* tslint:disable:no-console */
         // tslint:disable-next-line
         new ImperativeError({msg: "test"});
 
