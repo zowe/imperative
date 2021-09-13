00f71faf214480bc2e7c96b2042179ba7f5f23bb
Andrew W. Harn <andrew.harn@broadcom.com>
2021-09-13 14:54:27 -0400

    Commit what I have
    Signed-off-by: Andrew W. Harn <andrew.harn@broadcom.com>

---
+++ __tests__/__integration__/imperative/__tests__/__integration__/cli/config/import/cli.imperative-test-cli.config.import.integration.test.ts
@@ -0,0 +1,208 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { ITestEnvironment } from "../../../../../../../__src__/environment/doc/response/ITestEnvironment";
+import { SetupTestEnvironment } from "../../../../../../../__src__/environment/SetupTestEnvironment";
+import { runCliScript } from "../../../../../../../src/TestUtil";
+import * as fs from "fs";
+import * as path from "path";
+
+// Test Environment populated in the beforeAll();
+let TEST_ENVIRONMENT: ITestEnvironment;
+
+// Web addresses for all of the different configs
+const configAddress = "https://gist.githubusercontent.com/awharn/8a7d20941de6731ef92424ae2a4e1fd6/raw/b5310a3fab8ff03ac06d7adeeb5d0a0e5aa8f9df/test.config.good.with.schema.json";
+const configOnlyAddress = "https://gist.githubusercontent.com/awharn/8a7d20941de6731ef92424ae2a4e1fd6/raw/aac13e460892d2e11d3af9b775ab725cd3167a1b/test.config.good.without.schema.json";
+const badConfigAddress = "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/79dafd7c98e53f10eb668a29ddeb08ae5412d609/zowe.config.json.bad";
+const badAddress = "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/34cf180414061107ddb8b7f5a4e693b8fd7c2854/zowe.config.json.bad";
+
+describe("imperative-test-cli config import", () => {
+    // Create the test environment
+    beforeAll(async () => {
+        TEST_ENVIRONMENT = await SetupTestEnvironment.createTestEnv({
+            cliHomeEnvVar: "IMPERATIVE_TEST_CLI_CLI_HOME",
+            testName: "imperative_test_cli_test_config_init_command"
+        });
+    });
+    beforeEach(() => {
+        runCliScript(__dirname + "/../__scripts__/create_directory.sh", TEST_ENVIRONMENT.workingDir, ["fakeHome"]);
+        process.env.IMPERATIVE_TEST_CLI_CLI_HOME = path.join(TEST_ENVIRONMENT.workingDir, "fakeHome");
+    });
+    afterEach(() => {
+        runCliScript(__dirname + "/../__scripts__/delete_configs.sh", TEST_ENVIRONMENT.workingDir, ["-rf test fakeHome *.json"]);
+    });
+
+    describe("success scenarios", () => {
+
+        it("should display the help", () => {
+            const response = runCliScript(__dirname + "/../__scripts__/get_help.sh", TEST_ENVIRONMENT.workingDir, ["import"]);
+            const expectedLines = [
+                "Import config files from another location on disk or from an Internet URL.",
+                "File path or URL to import from.",
+                "Target the global config files.",
+                "Target the user config files.",
+                "Overwrite config file if one already exists."
+            ];
+            expectedLines.forEach((line: string) => expect(response.output.toString()).toContain(line));
+            expect(response.error).not.toBeDefined();
+            expect(response.stderr.toString()).toEqual("");
+        });
+    
+        it("should successfully import and overwrite a schema and config", () => {
+            let response = runCliScript(path.join(__dirname, "/__scripts__/import_config.sh"), TEST_ENVIRONMENT.workingDir, [
+                path.join(__dirname, "__resources__", "test.config.good.with.schema.json"), "--user-config false --global-config false"
+            ]);
+    
+            expect(response.stderr.toString()).toEqual("");
+            expect(response.status).toEqual(0);
+    
+            let config = fs.readFileSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json")).toString();
+            let schema = fs.readFileSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "test.schema.good.json")).toString();
+            expect(config).toEqual(fs.readFileSync(path.join(__dirname, "__resources__", "test.config.good.with.schema.json")).toString());
+            expect(schema).toEqual(fs.readFileSync(path.join(__dirname, "__resources__", "test.schema.good.json")).toString());
+    
+            response = runCliScript(path.join(__dirname, "/__scripts__/import_config_no_mkdir.sh"), TEST_ENVIRONMENT.workingDir, [
+                path.join(__dirname, "__resources__", "test.config.good.modified.with.schema.json"), "--user-config false --global-config false --ow"
+            ]);
+    
+            expect(response.stderr.toString()).toEqual("");
+            expect(response.status).toEqual(0);
+    
+            config = fs.readFileSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json")).toString();
+            schema = fs.readFileSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "test.schema.good.modified.json")).toString();
+            expect(config).toEqual(fs.readFileSync(path.join(__dirname, "__resources__", "test.config.good.modified.with.schema.json")).toString());
+            expect(schema).toEqual(fs.readFileSync(path.join(__dirname, "__resources__", "test.schema.good.modified.json")).toString());
+        });
+    
+        describe("from the web", () => {
+    
+            it("should successfully import a config from a URL", () => {
+                const response = runCliScript(path.join(__dirname, "/__scripts__/import_config.sh"), TEST_ENVIRONMENT.workingDir, [
+                    configOnlyAddress, "--user-config false --global-config false"
+                ]);
+    
+                expect(response.stderr.toString()).toEqual("");
+                expect(response.status).toEqual(0);
+    
+                const config = fs.readFileSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json")).toString();
+                expect(config).toEqual(fs.readFileSync(path.join(__dirname, "__resources__", "test.config.good.without.schema.json")).toString());
+                expect(fs.existsSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "test.schema.good.json"))).toEqual(false);
+            });
+    
+            it("should successfully import a config and schema from a URL", () => {
+                const response = runCliScript(path.join(__dirname, "/__scripts__/import_config.sh"), TEST_ENVIRONMENT.workingDir, [
+                    configAddress, "--user-config false --global-config false"
+                ]);
+        
+                expect(response.stderr.toString()).toEqual("");
+                expect(response.status).toEqual(0);
+        
+                const config = fs.readFileSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json")).toString();
+                const schema = fs.readFileSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "test.schema.good.json")).toString();
+                expect(config).toEqual(fs.readFileSync(path.join(__dirname, "__resources__", "test.config.good.with.schema.json")).toString());
+                expect(schema).toEqual(fs.readFileSync(path.join(__dirname, "__resources__", "test.schema.good.json")).toString());
+            });
+        });
+    
+        describe("from the disk", () => {
+    
+            it("should successfully import a config from a file", () => {
+                const response = runCliScript(path.join(__dirname, "/__scripts__/import_config.sh"), TEST_ENVIRONMENT.workingDir, [
+                    path.join(__dirname, "__resources__", "test.config.good.without.schema.json"), "--user-config false --global-config false"
+                ]);
+        
+                expect(response.stderr.toString()).toEqual("");
+                expect(response.status).toEqual(0);
+        
+                const config = fs.readFileSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json")).toString();
+                expect(config).toEqual(fs.readFileSync(path.join(__dirname, "__resources__", "test.config.good.without.schema.json")).toString());
+                expect(fs.existsSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "test.schema.good.json"))).toEqual(false);
+            });
+        
+            it("should successfully import a config and schema from a file", () => {
+                const response = runCliScript(path.join(__dirname, "/__scripts__/import_config.sh"), TEST_ENVIRONMENT.workingDir, [
+                    path.join(__dirname, "__resources__", "test.config.good.with.schema.json"), "--user-config false --global-config false"
+                ]);
+        
+                expect(response.stderr.toString()).toEqual("");
+                expect(response.status).toEqual(0);
+        
+                const config = fs.readFileSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json")).toString();
+                const schema = fs.readFileSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "test.schema.good.json")).toString();
+                expect(config).toEqual(fs.readFileSync(path.join(__dirname, "__resources__", "test.config.good.with.schema.json")).toString());
+                expect(schema).toEqual(fs.readFileSync(path.join(__dirname, "__resources__", "test.schema.good.json")).toString());
+            });
+        });
+    });
+
+    describe("failure scenarios", () => {
+        
+        describe("from the web", () => {
+
+            it("should fail to import a config from a bad URL", () => {
+                const response = runCliScript(path.join(__dirname, "/__scripts__/import_config.sh"), TEST_ENVIRONMENT.workingDir, [
+                    badAddress, "--user-config false --global-config false"
+                ]);
+        
+                expect(response.stdout.toString()).toEqual("");
+                expect(response.stderr.toString()).toContain("Rest API failure with HTTP(S) status");
+                expect(response.status).toEqual(1);
+        
+                expect(fs.existsSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json"))).toEqual(false);
+                expect(fs.existsSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "test.schema.good.json"))).toEqual(false);
+            });
+
+
+        });
+        
+        describe("from the disk", () => {
+
+        });
+    })
+    
+    
+    
+    });
+    // it("should fail to import a schema from a bad URL", () => {
+
+    // });
+    it("should fail to import a config from a bad path", () => {
+        const response = runCliScript(path.join(__dirname, "/__scripts__/import_config.sh"), TEST_ENVIRONMENT.workingDir, [
+            path.join(__dirname, "__resources__", "__fake__", "test.config.good.with.schema.json"),
+            "--user-config false --global-config false"
+        ]);
+
+        expect(response.stderr.toString()).toEqual("");
+        expect(response.stdout.toString()).toEqual("");
+        expect(response.status).toEqual(1);
+
+        expect(fs.existsSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "imperative-test-cli.config.json"))).toEqual(false);
+        expect(fs.existsSync(path.join(TEST_ENVIRONMENT.workingDir, "test", "test.schema.good.json"))).toEqual(false);
+    });
+    // it("should fail to import a schema from a bad path", () => {
+
+    // });
+    // it("should fail to import a config that is invalid JSON from a URL", () => {
+
+    // });
+    // it("should fail to import a config that is invalid JSON from a path", () => {
+
+    // });
+    // it("should fail to import a schema if it is defined with an absolute path", () => {
+
+    // });
+    // it("should fail to import a schema if it is defined with a URL", () => {
+
+    // });
+    // it("should fail to import a schema and config if they already exist", () => {
+
+    // });
+});
--- packages/imperative/__tests__/config/cmd/import/import.handler.test.ts
+++ packages/imperative/__tests__/config/cmd/import/import.handler.test.ts
@@ -12,32 +12,111 @@
 import ImportHandler from "../../../../src/config/cmd/import/import.handler";
 import { URL } from "url";
 import { join } from "path";
+import { IHandlerParameters } from "../../../../..";
+import { ImperativeConfig } from "../../../../../utilities";
+import { IImperativeConfig } from "../../../../src/doc/IImperativeConfig";
+import { Config } from "../../../../../config";
+import { CredentialManagerFactory } from "../../../../../security";
+import * as config from "../../../../../../__tests__/__integration__/imperative/src/imperative";
 import * as fs from "fs";
+import * as lodash from "lodash";
+import { Imperative } from "../../../../src/Imperative";
+
 
 const flushPromises = () => new Promise(setImmediate);
 
 const localSchemaDir: string = join(__dirname, "__resources__");
-const localSchema: string = join(localSchemaDir, "zowe.schema.json");
+const localSchema: string = join(localSchemaDir, "zowe.schema.json.original");
+const localConfig: string = join(localSchemaDir, "zowe.config.json.original");
 const localDownloadedSchemaPath = join(localSchemaDir, "__data__", "downloaded.schema.json");
 const localSchemaUrl: URL = new URL("file://" + localSchema);
 
-const configUrl: URL = new URL(
-    "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/79dafd7c98e53f10eb668a29ddeb08ae5412d609/zowe.config.json"
-);
-const schemaUrl: URL = new URL(
-    "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/79dafd7c98e53f10eb668a29ddeb08ae5412d609/zowe.schema.json"
-);
-const badConfigUrl: URL = new URL(
-    "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/79dafd7c98e53f10eb668a29ddeb08ae5412d609/zowe.config.json.bad"
-);
-const badSchemaUrl: URL = new URL(
-    "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/34cf180414061107ddb8b7f5a4e693b8fd7c2853/zowe.schema.json.bad"
-);
-const badUrl: URL = new URL(
-    "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/34cf180414061107ddb8b7f5a4e693b8fd7c2854/zowe.config.json.bad"
-);
+const configAddress = "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/79dafd7c98e53f10eb668a29ddeb08ae5412d609/zowe.config.json";
+const schemaAddress = "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/79dafd7c98e53f10eb668a29ddeb08ae5412d609/zowe.schema.json";
+const badConfigAddress = "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/79dafd7c98e53f10eb668a29ddeb08ae5412d609/zowe.config.json.bad";
+const badSchemaAddress = "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/34cf180414061107ddb8b7f5a4e693b8fd7c2853/zowe.schema.json.bad";
+const badAddress = "https://gist.githubusercontent.com/awharn/629aa52801a9a5f8b7f725b33572acf8/raw/34cf180414061107ddb8b7f5a4e693b8fd7c2854/zowe.config.json.bad";
+
+const configUrl: URL = new URL(configAddress);
+const schemaUrl: URL = new URL(schemaAddress);
+const badConfigUrl: URL = new URL(badConfigAddress);
+const badSchemaUrl: URL = new URL(badSchemaAddress);
+const badUrl: URL = new URL(badAddress);
 
 describe("Configuration import command handler", () => {
+
+    describe("handler", () => {
+
+        const fakeConfig = config as IImperativeConfig;
+
+        
+        let osHomedirSpy: any;
+        let currentWorkingDirectorySpy: any;
+        let fetchConfigSpy: any;
+        let downloadSchemaSpy: any;
+        let existsSyncSpy: any;
+        let readFileSyncSpy: any;
+
+        function getIHandlerParametersObject(): IHandlerParameters {
+            const x: any = {
+                response: {
+                    data: {
+                        setMessage: jest.fn((setMsgArgs) => {
+                            // Nothing
+                        }),
+                        setObj: jest.fn((setObjArgs) => {
+                            // Nothing
+                        })
+                    },
+                    console: {
+                        log: jest.fn((logs) => {
+                            // Nothing
+                        }),
+                        error: jest.fn((errors) => {
+                            // Nothing
+                        }),
+                        errorHeader: jest.fn(() => undefined)
+                    }
+                },
+                arguments: {
+                    globalConfig: false,
+                    userConfig: false,
+                    overwrite: false,
+                    location: undefined
+                }
+            };
+            return x as IHandlerParameters;
+        }
+
+        async function setupConfigToLoad() {
+            // Load the ImperativeConfig so import can work properly
+
+            osHomedirSpy.mockReturnValue(__dirname); // Pretend the current directory is the homedir
+            currentWorkingDirectorySpy.mockReturnValue(__dirname); // Pretend the current directory is where the command was invoked
+            ImperativeConfig.instance.config = await Config.load("fakeapp", {});
+        }
+
+        beforeEach( async () => {
+            jest.resetAllMocks();
+            ImperativeConfig.instance.loadedConfig = lodash.cloneDeep(fakeConfig);
+            Object.defineProperty(CredentialManagerFactory, "initialized", { get: () => true });
+            fetchConfigSpy = jest.spyOn((ImportHandler as any), "fetchConfig");
+            downloadSchemaSpy = jest.spyOn((ImportHandler as any), "downloadSchema");
+        });
+
+        it.skip("should be able to import from a web address", async () => {
+            const handler = new ImportHandler();
+            const params = getIHandlerParametersObject();
+
+            params.arguments.location = configAddress;
+            fetchConfigSpy.mockReturnValue(fs.readFileSync(localConfig));
+            readFileSyncSpy = jest.spyOn(fs, "readFileSync");
+            existsSyncSpy = jest.spyOn(fs, "existsSync");
+
+            await setupConfigToLoad();
+            await handler.process(params);
+        });
+    });
 
     describe("fetch config", () => {
         it("should be able to fetch the configuration file from a web address", async () => {
@@ -78,13 +157,18 @@ describe("Configuration import command handler", () => {
     });
 
     describe("download schema", () => {
-        afterEach(() => {
+        async function cleanup() {
             try {
                 fs.unlinkSync(localDownloadedSchemaPath);
-            } finally {
-                // do nothing
+            } catch (err) {
+                // Do nothing
             }
-        });
+        }
+
+        afterEach(async () => {
+            await cleanup();
+        })
+        
         it("should be able to copy the schema file from a local file", async () => {
             const downloadSchema = (ImportHandler.prototype as any).downloadSchema;
             await downloadSchema(localSchemaUrl, localDownloadedSchemaPath);
@@ -92,6 +176,40 @@ describe("Configuration import command handler", () => {
             const originalSchema = fs.readFileSync(localSchema).toString();
             const copiedSchema = fs.readFileSync(localDownloadedSchemaPath).toString();
             expect(copiedSchema).toEqual(originalSchema);
+        });
+
+        it("should handle errors encountered for an invalid file output path", async () => {
+            const downloadSchema = (ImportHandler.prototype as any).downloadSchema;
+            const badLocalDownloadedSchemaPath = join(__dirname, "__fake__", "__path__", "zowe.schema.json");
+            let error;
+
+            try {
+                await downloadSchema(localSchemaUrl, badLocalDownloadedSchemaPath);
+                await flushPromises();
+            } catch (err) {
+                error = err;
+            }
+
+            expect(fs.existsSync(badLocalDownloadedSchemaPath)).toEqual(false);
+            expect(error).toBeDefined();
+            expect(error.message).toContain("ENOENT: no such file or directory, copyfile"); // TODO: Should this be more.... clear?
+        });
+
+        it("should handle errors encountered for an invalid input path", async () => {
+            const downloadSchema = (ImportHandler.prototype as any).downloadSchema;
+            const badLocalSchemaUrl = new URL("file://" + join(__dirname, "__fake__", "__path__", "zowe.schema.json"));
+            let error;
+
+            try {
+                await downloadSchema(badLocalSchemaUrl, localDownloadedSchemaPath);
+                await flushPromises();
+            } catch (err) {
+                error = err;
+            }
+
+            expect(fs.existsSync(localDownloadedSchemaPath)).toEqual(false);
+            expect(error).toBeDefined();
+            expect(error.message).toContain("ENOENT: no such file or directory, copyfile"); // TODO: Should this be more.... clear?
         });
 
         it("should be able to download the schema file from a web address", async () => {
@@ -106,41 +224,16 @@ describe("Configuration import command handler", () => {
 
         it("should handle errors encountered for an invalid URL", async () => {
             const downloadSchema = (ImportHandler.prototype as any).downloadSchema;
-            await downloadSchema(badSchemaUrl, localDownloadedSchemaPath);
-            await flushPromises();
-            const originalSchema = fs.readFileSync(localSchema).toString();
-            const copiedSchema = fs.readFileSync(localDownloadedSchemaPath).toString();
-            expect(copiedSchema).not.toEqual(originalSchema);
-        });
-
-        it.skip("should handle errors encountered for an invalid file path", async () => {
-            const fetchConfig = (ImportHandler.prototype as any).fetchConfig;
-            let config: any;
-            let error: any;
+            let error;
             try {
-                config = await fetchConfig(badUrl);
+                await downloadSchema(badSchemaUrl, localDownloadedSchemaPath);
+                await flushPromises();
             } catch (err) {
                 error = err;
             }
-
-            expect(config).toBeUndefined();
             expect(error).toBeDefined();
             expect(error.message).toContain("Rest API failure with HTTP(S)");
-        });
-
-        it.skip("should handle errors encountered for an invalid output path", async () => {
-            const fetchConfig = (ImportHandler.prototype as any).fetchConfig;
-            let config: any;
-            let error: any;
-            try {
-                config = await fetchConfig(badUrl);
-            } catch (err) {
-                error = err;
-            }
-
-            expect(config).toBeUndefined();
-            expect(error).toBeDefined();
-            expect(error.message).toContain("Rest API failure with HTTP(S)");
+            expect(fs.existsSync(localDownloadedSchemaPath)).toEqual(true); // TODO: Uh, if it fails, shouldn't it NOT create a file???
         });
     });
 });
