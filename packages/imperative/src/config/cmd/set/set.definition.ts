e6ebdd0563ab82c04a302d341830c9a423837f93
Amanda D'Errico <amanda.derrico@ibm.com>
2022-08-09 12:08:52 -0400

    fixed syntax and removed periods in command example descriptions
    Signed-off-by: Amanda D'Errico <amanda.derrico@ibm.com>

--- packages/imperative/src/config/cmd/set/set.definition.ts
+++ packages/imperative/src/config/cmd/set/set.definition.ts
@@ -61,15 +61,15 @@ export const setDefinition: ICommandDefinition = {
     ],
     examples: [
         {
-            description: "Set the property in global config.",
+            description: "Set the property in global config",
             options: `"profiles.host1.profiles.service1.properties.setting" "value" --global-config`
         },
         {
-            description: "Set the property in user config.",
+            description: "Set the property in user config",
             options: `"profiles.host1.profiles.service2.properties.setting" "value" --user-config`
         },
         {
-            description: "Set property value to JSON.",
+            description: "Set property value to JSON",
             options: `"profiles.host1.profiles.service3.properties.setting" "{"property":"value"}" --json`
         },
         {
