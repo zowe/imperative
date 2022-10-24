89a1057805d101f7b3175a6daea38c478974a41d
Gene Johnston <eugene.johnston@broadcom.com>
2022-10-24 14:21:58 -0400

    Remove unused path module
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- packages/imperative/__tests__/config/cmd/report-env/EnvQuery.unit.test.ts
+++ packages/imperative/__tests__/config/cmd/report-env/EnvQuery.unit.test.ts
@@ -11,7 +11,6 @@
 
 import * as fs from "fs";
 import * as os from "os";
-import * as path from "path";
 
 import { CommandResponse } from "../../../../../cmd/src/response/CommandResponse";
 import { IO } from "../../../../../io";
