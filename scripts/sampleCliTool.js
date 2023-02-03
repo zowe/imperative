367cd2a9647e89698bbb8f949d6e39b16e2208ec
Timothy Johnson <timothy.johnson@broadcom.com>
2023-02-03 12:37:30 -0500

    Set npm_config_install_links to false
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- scripts/sampleCliTool.js
+++ scripts/sampleCliTool.js
@@ -32,7 +32,7 @@ switch (process.argv[2]) {
         runAll((dir) => ({ command: "npm run build", cwd: dir }), true);
         break;
     case "install":
-        runAll((dir) => ({ command: `npm install -g . --install-links false --prefix ${npmPrefix}`, cwd: dir }));
+        runAll((dir) => ({ command: `npm install -g . --prefix ${npmPrefix}`, cwd: dir }));
         break;
     case "uninstall":
         // Delete install folder since npm uninstall doesn't work as expected: https://github.com/npm/npm/issues/17905
