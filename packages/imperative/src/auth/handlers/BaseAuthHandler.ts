45e775a6112074a6be415bfe596bad079838c897
zFernand0 <37381190+zFernand0@users.noreply.github.com>
2023-06-26 14:45:49 +0000

    allow logout to remove token value event if token type is not defined and viceversa
    Signed-off-by: zFernand0 <37381190+zFernand0@users.noreply.github.com>

--- packages/imperative/src/auth/handlers/BaseAuthHandler.ts
+++ packages/imperative/src/auth/handlers/BaseAuthHandler.ts
@@ -230,8 +230,8 @@ export abstract class BaseAuthHandler extends AbstractAuthHandler {
             let profileWithToken: string = null;
 
             // If you specified a token on the command line, then don't delete the one in the profile if it doesn't match
-            if (Object.keys(profileProps).length > 0 && profileProps.tokenType != null && profileProps.tokenValue != null &&
-                profileProps.tokenType === params.arguments.tokenType && profileProps.tokenValue === params.arguments.tokenValue) {
+            if (Object.keys(profileProps).length > 0 && (profileProps.tokenType != null || profileProps.tokenValue != null) &&
+                (profileProps.tokenType === params.arguments.tokenType || profileProps.tokenValue === params.arguments.tokenValue)) {
                 const profilePath = config.api.profiles.expandPath(profileName);
                 config.delete(`${profilePath}.properties.tokenType`);
                 config.delete(`${profilePath}.properties.tokenValue`);
